142752487e16cd34b43e3228a50a68cb0fb1d517
bteicho@gmail.com <btegicho@gmail.com>
2024-08-13 19:53:10 +0200

    exercise two is finished

--- 2-Browsers/Week1/assignment/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/assignment/ex2-aboutMe/index.js
@@ -10,3 +10,22 @@ Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-B
 ------------------------------------------------------------------------------*/
 
 // TODO add your JavaScript code here.
+
+// 1. Using JavaScript, replace each of the spans (`nickname`, fav-food`, 
+//    `hometown`) with your own information.
+
+const nickname = document.getElementById('nickname');
+const myData = ['beni', 'chicken', 'istanbul'];
+const li = document.querySelectorAll('li');
+
+const spans= document.querySelectorAll('span');
+spans.forEach((span , index) =>{
+    span.textContent = myData[index];
+})
+// 2. In JavaScript, iterate through each `<li>` and change the class to 
+//    `list-item`.
+
+const listItems = document.querySelectorAll('li');
+listItems.forEach((item) => {
+    item.className = 'list-item';
+});
